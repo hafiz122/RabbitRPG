41e70ff6c13554976207d390c1468220b76db367
Syntaxable <35808181+hafiz122@users.noreply.github.com>
2018-02-03 13:47:59 +0800

    Update Command_awesomecommand.java

--- RabbitRPG/src/me/rabbitdegamer/rabbitrpg/commands/Command_awesomecommand.java
+++ RabbitRPG/src/me/rabbitdegamer/rabbitrpg/commands/Command_awesomecommand.java
@@ -12,7 +12,7 @@ public class Command_awesomecommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(cmd.getName().equalsIgnoreCase("awesomecommand") && sender instanceof Player);
 		Player s = (Player) sender;
-		Bukkit.getServer().broadcastMessage("<" + ChatColor.DARK_RED + s.getName() + "> " + ChatColor.WHITE + "Let's go to Japan!");
+		Bukkit.getServer().broadcastMessage("<" + ChatColor.DARK_RED + s.getName() + ChatColor.WHITE + "> " + ChatColor.WHITE + "Let's go to Japan!");
 		return true;
 		
 	}
